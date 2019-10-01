595c371eba0e0d4f3787c90bde18de5bd15ce470
Chandrakant <aryalinux11@gmail.com>
2019-10-01 07:04:22 +0530

    Cleaned up code to start from scratch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { EmployeeslistComponent } from './employeeslist/employeeslist.component';
-import { MatTableModule } from '@angular/material';
 import { HttpClientModule } from '@angular/common/http';
-import { UserService } from './user.service';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    EmployeeslistComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatTableModule,
     HttpClientModule
   ],
-  providers: [UserService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
